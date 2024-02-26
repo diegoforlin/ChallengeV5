141bada432c64b24fe9c6a129be7f229f6c08c7e
diegoforlin <135170390+diegoforlin@users.noreply.github.com>
2024-02-26 13:02:34 -0300

    Add files via upload

---
+++ src/main/java/com/challengescrd/challenge/auth/AuthenticationService.java
@@ -0,0 +1,29 @@
+package com.challengescrd.challenge.auth;
+
+import com.challengescrd.challenge.entities.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+
+    }
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception
+        {
+            return configuration.getAuthenticationManager();
+        }
+    }
